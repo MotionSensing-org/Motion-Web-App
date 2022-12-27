9ec6289f4c5abf060b3c91582d542d42bbdc4f7c
Orend92 <118512076+Orend92@users.noreply.github.com>
2022-12-27 10:53:39 +0200

    improve app styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:iot_project/LandingPage/chart_dash.dart';
-import 'package:iot_project/Navbar/Navbar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
@@ -29,6 +28,119 @@ class MyApp extends StatelessWidget {
         fontFamily: "Arial"
       ),
       home: ProviderScope(child: MyHomePage()),
+    );
+  }
+}
+
+class AlgParams extends ConsumerStatefulWidget{
+  Map properties;
+  AlgParams({super.key, required this.properties});
+
+  @override
+  ConsumerState<AlgParams> createState() => _AlgParams();
+}
+
+class _AlgParams extends ConsumerState<AlgParams>{
+  String? dropdownValue = '';
+  @override
+  Widget build(BuildContext context) {
+    List curAlgParams = ref.watch(requestAnswerProvider).curAlgParams;
+    String currentAlgorithm = ref.watch(chosenAlgorithmProvider).chosenAlg;
+    List<Widget> params = [];
+
+    if(currentAlgorithm != '') {
+      for(int i = 0; i < curAlgParams.length ;i++){
+        if(curAlgParams[i]['type'] == 'TextBox'){
+          if(!widget.properties.containsKey(curAlgParams[i]['param_name'])) {
+            widget.properties[curAlgParams[i]['param_name']] = curAlgParams[i]['default_value'];
+          }
+
+          params.add(Card(
+            color: Colors.white,
+            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(4))),
+            child: TextField(
+              onChanged: (value){
+                widget.properties[curAlgParams[i]['param_name']] = value;
+              },
+              decoration: InputDecoration(
+                labelText: curAlgParams[i]['param_name'],
+                border: const OutlineInputBorder(),
+                hintText: 'Enter a value',
+              ),
+            ),
+          )
+          );
+        } else {
+          if(curAlgParams[i]['type'] == 'CheckList') {
+            List vals = curAlgParams[i]['values'];
+            if(dropdownValue == '') {
+              dropdownValue = vals[curAlgParams[i]['default_value']];
+              widget.properties[curAlgParams[i]['param_name']] = dropdownValue;
+            }
+
+            params.add(
+              Column(
+                children: [
+                  Text(
+                    curAlgParams[i]['param_name'],
+                    style: const TextStyle(color: Colors.blue),
+                  ),
+                  Card(
+                    color: Colors.white,
+                    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: DropdownButton(
+                          items: [for(int k = 0; k < vals.length; k++) DropdownMenuItem<String>(
+                            value: vals[k],
+                            child: Text(vals[k]),
+                          )],
+                          icon: const Icon(
+                            Icons.arrow_downward,
+                            color: Colors.blue,
+                          ),
+                          value: dropdownValue,
+                          underline: Container(
+                            color: Colors.blue,
+                            height: 3,
+                          ),
+                          style: const TextStyle(color: Colors.blue),
+                          onChanged: (String? value) {
+                            setState(() {
+                              dropdownValue = value;
+                              widget.properties[curAlgParams[i]['param_name']] = dropdownValue;
+                            });
+                          }
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        }
+      }
+    }
+
+    params.add(
+        Card(
+          color: Colors.white,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: TextButton(
+            child: const Text(
+              'Start',
+              style: TextStyle(
+                color: Colors.green
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => ProviderScope(child: ChartDashRoute(properties: widget.properties,))));
+            },
+          ),
+        )
+    );
+    return Column(
+      children: params,
     );
   }
 }
@@ -102,7 +214,7 @@ class _DashControl extends ConsumerState<DashControl> {
                 child: AnimatedCrossFade(
                     crossFadeState: chosenAlgorithm == '' ? CrossFadeState.showFirst
                         : CrossFadeState.showSecond,
-                    duration: const Duration(milliseconds: 500),
+                    duration: const Duration(milliseconds: 300),
                     firstChild: const SizedBox.shrink(),
                     secondChild: AlgParams(properties: widget.properties,)
                 ),
@@ -122,40 +234,81 @@ class ChartDashRoute extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     List<Widget> displayItems = [];
+    List<Widget> algParams = [];
     properties.forEach((key, value) {
       if(key == 'alg_name') {
-        displayItems.add(Text(value));
+        algParams.add(Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+              value,
+            style: const TextStyle(
+              color: Colors.blue,
+              fontSize: 20
+            ),
+          ),
+        ));
       } else {
-        displayItems.add(Text('$key: $value'));
+        algParams.add(Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+              '$key: $value',
+              style: const TextStyle(
+                  color: Colors.blue,
+                  // fontSize: 20
+              )
+          ),
+        ));
       }
     });
 
     displayItems.addAll([
-      ElevatedButton(
-        onPressed: () {
-          ref.read(playPauseProvider).playPause();
-        },
-        style: ButtonStyle(
-          shape: MaterialStateProperty.all(const CircleBorder()),
-          padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
-          // elevation: MaterialStateProperty.resolveWith<double?>((states) {
-          //   return 10; // <-- Splash color
-          // }),
-          backgroundColor: MaterialStateProperty.all(Colors.blue), // <-- Button color
-          overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
-            if (states.contains(MaterialState.pressed)) return Colors.lightBlueAccent; // <-- Splash color
-            return Colors.blue;
-          }),
+      Card(
+        color: Colors.white,
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: Column(
+          children: algParams,
         ),
-        child: ref.watch(playPauseProvider).pause
-            ? const Icon(Icons.pause_outlined)
-            : const Icon(Icons.play_arrow),
       ),
-      TextButton(
-        child: const Text('Stop'),
-        onPressed: () {
-          Navigator.pop(context);
-        },
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ElevatedButton(
+          onPressed: () {
+            ref.read(playPauseProvider).playPause();
+          },
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(const CircleBorder()),
+            padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+            // elevation: MaterialStateProperty.resolveWith<double?>((states) {
+            //   return 10; // <-- Splash color
+            // }),
+            backgroundColor: MaterialStateProperty.all(Colors.blue), // <-- Button color
+            overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
+              if (states.contains(MaterialState.pressed)) return Colors.lightBlueAccent; // <-- Splash color
+              return Colors.blue;
+            }),
+          ),
+          child: ref.watch(playPauseProvider).pause
+              ? const Icon(Icons.pause_outlined)
+              : const Icon(Icons.play_arrow),
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          color: Colors.white,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+          child: TextButton(
+            child: const Text(
+              'Stop',
+              style: TextStyle(
+                  color: Colors.red
+              ),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
       )
     ]);
 
@@ -171,8 +324,12 @@ class ChartDashRoute extends ConsumerWidget {
                   shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
                   elevation: 10,
                   color: Colors.lightBlueAccent,
-                  child: Column(
-                    children: displayItems,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: displayItems,
+                    ),
                   ),
                 ),
               )
@@ -208,22 +365,44 @@ class MyHomePage extends StatelessWidget {
                     elevation: 20,
                     color: Colors.lightBlueAccent,
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         DashControl(properties: properties,),
-                        TextButton(
-                          child: const Text('Start'),
-                          onPressed: () {
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => ProviderScope(child: ChartDashRoute(properties: properties,))));
-                          },
-                        )
                       ],
                     ),
                   ),
                 )
             ),
-            const Expanded(
+            Expanded(
               flex: 4,
-                child: Image(image: AssetImage('assets/images/ms-image-one.png'))
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 10),
+                  child: Card(
+                    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                    elevation: 20,
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                              'Welcome to the motion sensing web app!',
+                              style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.blue
+                              ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.all(60.0)),
+                        Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Image(image: AssetImage('assets/images/ms-image-one.png')),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
             )
           ],
         ),
