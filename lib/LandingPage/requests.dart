a84fcaf37c2844da1a3a17ff1b22ebd1d7d347c1
Orend92 <118512076+Orend92@users.noreply.github.com>
2023-02-22 03:06:15 +0200

    Revert "Update requests.dart"
    This reverts commit 5fa6a29755b0ed90875d70653bd5ec7c7c9d24dc.

--- lib/LandingPage/requests.dart
+++ lib/LandingPage/requests.dart
@@ -1,40 +1,43 @@
-import 'dart:io'; 
-import 'package:http/http.dart';  
-import '../consts.dart'; 
+import 'dart:io';
 
-Future writeToServer({required String query, required String body}) async {  // Defining a function that will send a POST request to the server
-  const String queryPrefix = '?request_type=';  // Declaring a constant variable with a string value
-  final String url = '$backendUrl/$queryPrefix$query';  // Creating a string that represents the complete URL for the server request
-  Response response = await post(Uri.parse(url),  // Sending a POST request to the server
-      headers: {"Content-Type": "application/json"},  // Specifying the headers of the request
-      body: body);  // Including the body of the request in the POST request
-  return response.body;  // Returning the response body from the server
+import 'package:http/http.dart';
+
+import '../consts.dart';
+
+Future writeToServer({required String query, required String body}) async {
+  const String queryPrefix = '?request_type=';
+  final String url = '$backendUrl/$queryPrefix$query';
+  Response response = await post(Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      body: body);
+  return response.body;
 }
 
-Future getData({required String query}) async {  // Defining a function that will send a GET request to the server
-  const String queryPrefix = '?request_type=';  // Declaring a constant variable with a string value
-  final String url = '$backendUrl/$queryPrefix$query';  // Creating a string that represents the complete URL for the server request
-  Response response = await get(Uri.parse(url));  // Sending a GET request to the server
-  return response.body;  // Returning the response body from the server
+Future getData({required String query}) async {
+  const String queryPrefix = '?request_type=';
+  final String url = '$backendUrl/$queryPrefix$query';
+  Response response = await get(Uri.parse(url));
+  return response.body;
 }
 
-Future getDataStream() async {  // Defining a function that will send a GET request to the server to get a data stream
-  const String url = '$backendUrl/stream';  // Creating a string that represents the complete URL for the server request
-  Response response = await get(Uri.parse(url));  // Sending a GET request to the server
-  return response.body;  // Returning the response body from the server
+Future getDataStream() async {
+  const String url = '$backendUrl/stream';
+  Response response = await get(Uri.parse(url));
+  return response.body;
 }
 
-Future<String> getPythonScriptPath() async {  // Defining a function that will return the path of a Python script
-  var files = await Directory.current.list(recursive: true).toList();  // Getting a list of files in the current directory and all subdirectories
+Future<String> getPythonScriptPath() async {
+  var files = await Directory.current.list(recursive: true).toList();
 
-  for (var file in files) {  // Looping through each file in the list
-    String path = file.path;  // Getting the path of the file
-    String name = path.split('\\').last;  // Getting the name of the file by splitting the path at the backslashes and taking the last element
-    if (file is File && name == 'app.py') {  // Checking if the file is a Python script named 'app.py'
-      return path;  // Returning the path of the Python script
+  for (var file in files) {
+    String path = file.path;
+    String name = path.split('\\').last;
+    if (file is File && name == 'app.py') {
+      return path;
     }
   }
 
-  return '';  // Returning an empty string if no Python script is found
+  return '';
 
-  // return 'C:\\Users\\odztm\\PycharmProjects\\flaskProject\\app.py';  
+  // return 'C:\\Users\\odztm\\PycharmProjects\\flaskProject\\app.py';
+}
